3d92775fe34f673007bab2482138e33e40c6ef56
jgfrazie <james.frazier8093@gmail.com>
2023-11-06 22:22:11 -0500

    Header nearly complete

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+
+let zoomLevel = 1.0;
+
+function themeSwitcher() {
+    document.body.classList.toggle('darkTheme');
+    document.body.classList.toggle('lightTheme');
+
+    const className = document.body.className;
+    if(className == "lightTheme") {
+        this.textContent = "Dark Theme";
+    } else {
+        this.textContent = "Light Theme";
+    }
+
+    console.log('current theme class: ' + className);
+}
+
+// function graphicalZoom(key) {
+//     if (key.code == '=') {
+//         zoomLevel += 0.1;
+
+//     }
+// }
+
+// function graphicalZoomEnabler() {
+//     document.addEventListener('keydown', graphicalZoom)
+// }
+
+/*
+Upon loading the page, will set the click event listener for the theme button.
+*/
+$(document).ready(function() {
+    document.querySelector('#themeButton').addEventListener('click', themeSwitcher);
+    // document.querySelector('#zoomButton').addEventListener('click', graphicalZoomEnabler);
+});
